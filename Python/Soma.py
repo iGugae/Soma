15d3468b194e1778535ef7d99e906569ca84f5c1
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2024-04-24 17:04:34 -0300

    Soma em Python

---
+++ Python/Soma.py
@@ -0,0 +1,9 @@
+#Variáveis
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo: '))
+
+#Cálculo
+soma = n1 + n2
+
+#Resultado
+print(f'A soma entre {n1} + {n2} é igual a {soma}.')
