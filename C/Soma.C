770f7e442b6637035d0babe5c19d1a0cd8fb13f0
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2024-04-24 17:08:49 -0300

    Soma em C

---
+++ C/Soma.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    //Variáveis
+    int a, b, soma;
+
+    //Entrada de dados
+    printf("Digite um numero: ");
+    scanf("%d",&a);
+    printf("Digite outro numero: ");
+    scanf("%d",&b);
+
+    //Cálculo
+    soma = a + b;
+    
+    //Saída de dados
+    printf("A soma entre %d + %d é igual a %d", a, b, soma);
+}
